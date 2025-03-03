a4c9328fc5f2d88fe4dfd38ee7c9e87782ea1eca
sbaby171 <maximillian.sbabo@gmail.com>
2025-03-02 18:34:36 -0800

    base support of Ollama Client and HuggingFace's InferenceClient

--- mlhq/mlhq/backend/openai.py
+++ mlhq/mlhq/backend/openai.py
@@ -5,43 +5,11 @@ from huggingface_hub import InferenceClient
 import os
 
 # --------------------------------------------------------------------|-------:
-class HFClient: 
-    def __init__(self, *args, **kwargs): 
-        self._backend = "huggingface"
-        
-    def chat(self, model, messages, stream=False): 
-        tokenizer = AutoTokenizer.from_pretrained(model)
-        model = AutoModelForCausalLM.from_pretrained(model) 
-        #return model(messages)
-        # Tokenize the input messages and return the result in tensor form
-        #inputs = tokenizer(messages, return_tensors="pt", padding=True, truncation=True)
-        #
-        ## Forward the tokenized inputs to the model and get the response
-        #outputs = model.generate(**inputs, max_length=100)  # Use model.generate for generation
-        #
-        # Decode the model's output tokens back to text
-        #response = tokenizer.decode(outputs[0], skip_special_tokens=True)
-        #return response
-        # Extract the 'content' from each message in the list of dictionaries
-        message_texts = [message['content'] for message in messages]
-        
-        # Tokenize the list of message texts
-        inputs = tokenizer(message_texts, return_tensors="pt", padding=True, truncation=True)
-        
-        # Generate model response using tokenized inputs
-        outputs = model.generate(**inputs, max_length=100)
-        
-        # Decode the model's output tokens back to text
-        response = tokenizer.decode(outputs[0], skip_special_tokens=True)
-        
-        return response
-
-        
-
 class Client: 
     def __init__(self, *args, **kwargs): 
         self._client = None 
         self._backend = kwargs.get('backend')
+        self._model = kwargs.get('model')
         if not self._backend: 
             raise RuntimeError("Must provide 'backend' at constructor.")
         if self._backend == "ollama": 
@@ -49,18 +17,21 @@ class Client:
         elif self._backend == "vllm": 
             raise ValueError(f"Not supporting {self._backend} backend - atm.")
         elif self._backend in ["huggingface", "hf"]: 
-            #raise ValueError(f"Not supporting {self._backend} backend - atm.")
-            #self._client = HFClient()
-            self._client = InferenceClient()
+            self._client = InferenceClient(model=self._model, token=os.environ['HF_TOKEN'])
             self._backend = "huggingface"
         else: 
             raise ValueError(f"Unrecognized {self._backend} backend")
 
 
     def chat(self , model, messages, stream=False): 
-        if self._backend == "ollama": 
+    #def chat(self , *args, **kwargs): 
+        if isinstance(self._client, ollama.Client): 
+            print(f"DEBUG: [chat] - Ollamabakend")
             return self._client.chat(model, messages, stream=stream)
-        elif self._backend == "huggingface": 
-            return self._client.chat.completions.create(model=model, messages=messages, stream=stream)
+        elif isinstance(self._client, InferenceClient): 
+            #response = self._client.chat_completion(messages, max_tokens=100)
+            response = self._client.chat_completion(messages, max_tokens=100, stream=stream)
+            return response
+            #return self._client.chat.completions.create(model=model, messages=messages, stream=stream)
       
 # --------------------------------------------------------------------|-------:
--- mlhq/sample.py
+++ mlhq/sample.py
@@ -1,10 +1,18 @@
 from mlhq.backend.openai import Client 
 # ^^^ improve to: from mlhq import Client
 import argparse 
+import sys 
 # --------------------------------------------------------------------|-------:
-DEFAULT_MODEL = "meta-llama/Llama-3.2-3B-Instruct"
+DEFAULT_MODEL = "meta-llama/Llama-3.1-8B-Instruct"
 DEFAULT_BACKEND = "hf"
 DEFAULT_PROMPT = "Hello, who are you?"
+
+HF_MODELS = [
+    "meta-llama/Llama-3.1-8B-Instruct", 
+]
+OLLAMA_MODELS = [
+    "deepseek-r1:8b", 
+]
 # --------------------------------------------------------------------|-------:
 def __handle_cli_args():
     parser = argparse.ArgumentParser()
@@ -15,19 +23,32 @@ def __handle_cli_args():
     parser.add_argument('--no-stream', dest='stream', action='store_false')
     parser.set_defaults(stream=True)
     args = parser.parse_args()
-    return args
 
+    if args.model in HF_MODELS: 
+        args.backend = "huggingface" 
+    elif args.model in OLLAMA_MODELS: 
+        args.backend = "ollama"
+    # ^^^ NOTE - this check could be pushed into mlhq.backend.openai.py
+    return args
 # --------------------------------------------------------------------|-------:
 def main(args): 
-     client = Client(backend=args.backend)
+     print(f"DEBUG: [main] Received model: {args.model}")
+     print(f"DEBUG: [main] Received backend: {args.backend}")
+     #sys.exit(1)
+     client = Client(model = args.model, backend=args.backend)
      response = client.chat(
          model = args.model, 
          messages = [{'role':'user', 'content': f"{args.prompt}"}],
          stream=args.stream, 
      )
      if args.stream: 
-         for chunk in response:
-             print(chunk['message']['content'], end='', flush=True)
+         if args.backend == "ollama": 
+             for chunk in response:
+                 print(chunk['message']['content'], end='', flush=True)
+         elif args.backend == "huggingface": 
+             for chunk in response: 
+                 #print(chunk.content, end='', flush=True)
+                 print(chunk.choices[0].delta.content, end='', flush=True)
      else: 
          print(response)
 # --------------------------------------------------------------------|-------:
