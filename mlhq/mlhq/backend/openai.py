0691328e81bf087ee6ea231975a06bbd80b6010c
sbaby171 <maximillian.sbabo@gmail.com>
2025-03-02 19:58:26 -0800

    updated backend to not initial Client with model but rather wait till chat call

--- mlhq/mlhq/backend/openai.py
+++ mlhq/mlhq/backend/openai.py
@@ -5,6 +5,10 @@ from huggingface_hub import InferenceClient
 import os
 
 # --------------------------------------------------------------------|-------:
+# Note - Its probably the case that model is not declared until the 
+#        chat functino within the OpenAI API because users could swap
+#        to different models on a given prompt. Therefore, we should 
+#        follow this and change only for good reason.
 class Client: 
     def __init__(self, *args, **kwargs): 
         self._client = None 
@@ -17,20 +21,21 @@ class Client:
         elif self._backend == "vllm": 
             raise ValueError(f"Not supporting {self._backend} backend - atm.")
         elif self._backend in ["huggingface", "hf"]: 
-            self._client = InferenceClient(model=self._model, token=os.environ['HF_TOKEN'])
+            self._client = InferenceClient(token=os.environ['HF_TOKEN'])
             self._backend = "huggingface"
         else: 
             raise ValueError(f"Unrecognized {self._backend} backend")
 
 
-    def chat(self , model, messages, stream=False): 
+    def chat(self , model, messages, stream=True): 
     #def chat(self , *args, **kwargs): 
         if isinstance(self._client, ollama.Client): 
-            print(f"DEBUG: [chat] - Ollamabakend")
-            return self._client.chat(model, messages, stream=stream)
+            return self._client.chat(self._model, messages, stream=stream)
+            #return self._client.chat(messages, stream=stream)
         elif isinstance(self._client, InferenceClient): 
             #response = self._client.chat_completion(messages, max_tokens=100)
-            response = self._client.chat_completion(messages, max_tokens=100, stream=stream)
+            #response = self._client.chat_completion(messages, max_tokens=100, stream=stream)
+            response = self._client.chat_completion(model=model, messages=messages, max_tokens=100, stream=stream)
             return response
             #return self._client.chat.completions.create(model=model, messages=messages, stream=stream)
       
