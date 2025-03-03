f2c94b744250463980067ad582762575c4eff12a
sbaby171 <maximillian.sbabo@gmail.com>
2025-03-02 18:36:31 -0800

    sample execution scripts

---
+++ mlhq/scripts/sample_calls.sh
@@ -0,0 +1,8 @@
+# Llama-3.1-8B on HF InferenceClient
+#python sample.py --model meta-llama/Llama-3.1-8B-Instruct --prompt="Who wrote the book '1984'?" 
+#python sample.py --model meta-llama/Llama-3.1-8B-Instruct --prompt="Who wrote the book '1984'?" --no-stream
+
+# Deepseek-R1-8B on Ollama 
+#python sample.py --model deepseek-r1:8b --prompt="Who wrote the book '1984'?"
+#python sample.py --model deepseek-r1:8b --prompt="Who wrote the book '1984'?" --no-stream
+
